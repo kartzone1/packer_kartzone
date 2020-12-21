a5d0bc4dc60c0ca9f571dd186caa244cf8e1e969
kartnico <kartnico@kartzone.email>
2020-12-21 20:44:01 +0100

    fix : correct msi name in install-cloud-init.ps1

--- windows10/install-cloud-init.ps1
+++ windows10/install-cloud-init.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -UseBasicParsing https://cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi -OutFile C:\Windows\Temp\cloudbaseinit.msi
-msiexec /i C:\Windows\Temp\cloudbaseinit.msi.msi /qn /l*v log.txt
-Remove-Item C:\Windows\Temp\cloudbaseinit.msi.msi
+msiexec /i C:\Windows\Temp\cloudbaseinit.msi /qn /l*v log.txt
+Remove-Item C:\Windows\Temp\cloudbaseinit.msi
