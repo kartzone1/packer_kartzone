c169a49ed3532df83a9211c2fcc00189e2dece58
kartnico <kartnico@kartzone.email>
2020-12-24 13:43:18 +0100

    manage minion_id on windows10

---
+++ windows10/config-salt-minionid.ps1
@@ -0,0 +1,9 @@
+$filePath = 'c:\salt\conf\minion_id'
+$tempFilePath = "$env:TEMP\$($filePath | Split-Path -Leaf)"
+$find = 'packerimg'
+$salthostname=[System.Net.Dns]::GetHostByName($env:computerName).HostName
+
+(Get-Content -Path $filePath) -replace $find, $salthostname | Add-Content -Path $tempFilePath
+
+Remove-Item -Path $filePath
+Move-Item -Path $tempFilePath -Destination $filePath
--- windows10/install-cloud-init.ps1
+++ windows10/install-cloud-init.ps1
@@ -3,3 +3,4 @@ cd ${env:SystemRoot}\Temp\
 Start-Process "${env:SystemRoot}\System32\msiexec.exe" -ArgumentList "/i ${env:SystemRoot}\Temp\cloudbaseinit.msi /qn /l*v log.txt INJECTMETADATAPASSWORD='0'" -wait
 $unattendedXmlPath = "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
 Add-Content "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf" "metadata_services=cloudbaseinit.metadata.services.nocloudservice.NoCloudConfigDriveService"
+Copy-Item "e:\config-salt-minionid.ps1" -Destination "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\LocalScripts"
