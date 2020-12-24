72addd49ca4d0df933c03db5cd7c07bd9a3b3818
kartnico <kartnico@kartzone.email>
2020-12-24 02:43:42 +0100

    Cloud init configuration

--- windows10/install-cloud-init.ps1
+++ windows10/install-cloud-init.ps1
@@ -1,3 +1,5 @@
-Invoke-WebRequest -UseBasicParsing https://cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi -OutFile C:\Windows\Temp\cloudbaseinit.msi
-msiexec /i C:\Windows\Temp\cloudbaseinit.msi /qn /l*v log.txt
-Remove-Item C:\Windows\Temp\cloudbaseinit.msi
+Invoke-WebRequest -Uri https://cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi -OutFile ${env:SystemRoot}\Temp\cloudbaseinit.msi
+cd ${env:SystemRoot}\Temp\
+Start-Process "${env:SystemRoot}\System32\msiexec.exe" -ArgumentList "/i ${env:SystemRoot}\Temp\cloudbaseinit.msi /qn /l*v log.txt INJECTMETADATAPASSWORD='0'" -wait
+$unattendedXmlPath = "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
+Add-Content "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf" "metadata_services=cloudbaseinit.metadata.services.nocloudservice.NoCloudConfigDriveService"
