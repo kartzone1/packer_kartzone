7c85b592cac2c0f542d7e5d645b63d5450894e9f
kartnico <kartnico@kartzone.email>
2020-12-29 00:02:04 +0100

    Add norestart to msi install + manage NoCloudConfigDriveService during unattended Cloudbase Init

--- windows10/install-cloud-init.ps1
+++ windows10/install-cloud-init.ps1
@@ -1,6 +1,9 @@
 Invoke-WebRequest -Uri https://cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi -OutFile ${env:SystemRoot}\Temp\cloudbaseinit.msi
 cd ${env:SystemRoot}\Temp\
-Start-Process "${env:SystemRoot}\System32\msiexec.exe" -ArgumentList "/i ${env:SystemRoot}\Temp\cloudbaseinit.msi /qn /l*v log.txt INJECTMETADATAPASSWORD='0'" -wait
-$unattendedXmlPath = "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
+Start-Process "${env:SystemRoot}\System32\msiexec.exe" -ArgumentList "/i ${env:SystemRoot}\Temp\cloudbaseinit.msi /norestart /qn /l*v log.txt INJECTMETADATAPASSWORD='0'" -wait
 Add-Content "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf" "metadata_services=cloudbaseinit.metadata.services.nocloudservice.NoCloudConfigDriveService"
+$file = "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init-unattend.conf"
+$line = Get-Content $file | Select-String metadata_services | Select-Object -ExpandProperty Line
+$content = Get-Content $file
+$content | ForEach-Object {$_ -replace $line,"metadata_services=cloudbaseinit.metadata.services.nocloudservice.NoCloudConfigDriveService"} | Set-Content $file
 Copy-Item "e:\config-salt-minionid.ps1" -Destination "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\LocalScripts"
